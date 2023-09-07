99994d71cb686e0eacb28ee672f858e80ffdd787
Douglas Ferreira <douglas0003@hotmail.com>
2023-09-06 22:12:45 -0300

    commit projeto base

---
+++ testes01/src/main/java/br/com/estudos/service/TesteService.java
@@ -0,0 +1,10 @@
+package br.com.estudos.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TesteService {
+    public void mensagemInicial(){
+        System.out.println("Olá Mundo - Douglas");
+    }
+}
