9591b12b13a398aa1387daae40041c7361103331
Thiago <tlucenalive@gmail.com>
2023-09-12 15:39:26 -0300

    Exercicio em andamento mostrando print de tela

--- testes01/src/main/java/br/com/estudos/Testes01Application.java
+++ testes01/src/main/java/br/com/estudos/Testes01Application.java
@@ -7,6 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
+//importando bibliotecas
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class Testes01Application {
 
@@ -20,6 +27,23 @@ public class Testes01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Testes01Application.class, args);
+		String caminhoArquivo = "C:/Teste01/teste01.txt";
+				
+				try {
+		            // Use a API java.nio.file para obter um Stream das linhas do arquivo
+		            Path path = Paths.get(caminhoArquivo);
+		            Stream<String> linhas = Files.lines(path);
+
+		            // Use forEach para imprimir cada linha no console
+		            linhas.forEach(System.out::println);
+
+		            // Feche o Stream após a leitura
+		            linhas.close();
+		        } catch (IOException e) {
+		            System.out.println("Ocorreu um erro ao ler o arquivo: " + e.getMessage());
+		        }
 	}
+	
 
 }
+
